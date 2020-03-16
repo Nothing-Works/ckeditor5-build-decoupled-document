221c80a0775728a5b774be26946539d7396aa7ff
AndySong <andylauszp@gmail.com>
2020-03-16 14:21:23 +1300

    wip

--- src/plugins/conversion/adddata.js
+++ src/plugins/conversion/adddata.js
@@ -3,20 +3,25 @@ import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 
 export default class AddData extends Plugin {
 	init() {
-		const editor = this.editor;
 
-		editor.conversion.for( 'dataDowncast' ).add( dispatcher => {
-			dispatcher.on( 'insert:paragraph', ( evt, data, conversionApi ) => {
-				if ( conversionApi.consumable.consume( data.item, 'insert' ) ) {
-					return;
-				}
-				const viewPosition = conversionApi.mapper.toViewPosition( data.range.start );
-				const viewElement = conversionApi.writer.createContainerElement( 'a' );
-				conversionApi.mapper.bindElements( data.item, viewElement );
-				conversionApi.writer.insert( viewPosition, viewElement );
-				evt.stop(); }
-			);
-		} );
+		// const editor = this.editor;
+
+		// editor.model.change( writer => {
+		// 	writer.setAttribute( 'foo', 'bar', '$root' );
+		// 	console.log( 'test' );
+		// } );
+		// editor.conversion.for( 'dataDowncast' ).add( dispatcher => {
+		// 	dispatcher.on( 'insert:paragraph', ( evt, data, conversionApi ) => {
+		// 		if ( conversionApi.consumable.consume( data.item, 'insert' ) ) {
+		// 			return;
+		// 		}
+		// 		const viewPosition = conversionApi.mapper.toViewPosition( data.range.start );
+		// 		const viewElement = conversionApi.writer.createContainerElement( 'a' );
+		// 		conversionApi.mapper.bindElements( data.item, viewElement );
+		// 		conversionApi.writer.insert( viewPosition, viewElement );
+		// 		evt.stop(); }
+		// 	);
+		// } );
 		// Editor.prototype.Andy = () => {
 		// 	return this.editor.getData() + 'juele';
 		// };
